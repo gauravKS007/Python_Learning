26072a164e20487b6d517aa758e209af8217a0da
admin <gauravsinghgtc@gmail.com>
2024-07-25 17:40:59 +0530

    basic code1

---
+++ basic.py
@@ -0,0 +1,53 @@
+"""mystring ="Hello World"
+print(mystring[:9])
+
+mystring ="Hello World"
+print(mystring[2:9])"""
+
+"""mystring ="Hello World"
+print(mystring[::-1])"""
+
+"""mystring ="abcdefghij"
+print(mystring[2:8:3])"""
+
+#mystring ="abcdefghij"
+#print(mystring[::])
+
+# abc ="Sam"
+# x='Sam'[1:]
+# print(x)
+# x ='P'+x
+# print(x)
+
+# x="hello world"
+# x=x+" is beautiful"
+# print(x)
+
+# let='z'
+# print(let*10)
+
+a ="Gaur,av sin,gh"
+
+# print(a.split(","))
+
+
+# print('this {2} {0} {1}'.format("Hi","hello","by"))
+#python <3.6
+# name = "gaurav"
+# print("this is me {}".format(name))
+
+#python 3.6
+# print(f'this is me {name}')
+name ="rohan"
+last_name="mehra"
+print(f"{name} is from {last_name} family")
+#String formating
+# print("this {b} {a} {c}".format(a="gaurav",b="is",c="kumar"))
+
+
+#Float FORMATING {value:width.precision f}
+
+# result = 100/7
+# print("result = {r:0.8f}".format(r=result) )
+#hello.
+
